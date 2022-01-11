bc69e0594beb25402b74acb5ad673405ac8b5b8f
Laetitia Taddei <laetdi@gmail.com>
2022-01-11 21:30:17 +0900

    index route steup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning DB"
+puts 'Cleaning DB'
 Restaurant.destroy_all
 
 puts 'DB is clean'
@@ -20,4 +20,4 @@ puts 'DB is clean'
   puts "restaurant #{restaurant.id} is created"
 end
 
-puts "done"
+puts 'done'
