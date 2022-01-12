b2003a4774b57beeb5cc9e4f9dab1723c2e0c9ad
Laetitia Taddei <laetdi@gmail.com>
2022-01-12 09:44:02 +0900

    new and create path + stars rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+
+  def blank_stars
+    5 - rating.to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :restaurants, except: [ :new, :create, :destroy]
   resources :restaurants do
     resources :reviews, only: [ :new, :create ]
   end
